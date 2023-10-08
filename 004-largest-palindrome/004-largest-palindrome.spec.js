e9f58435921c7d76b46513fd909b989b11bcaa0e
Austin Talbot <austintalbot@gmail.com>
2023-10-08 07:20:42 -0600

    fix test

--- 004-largest-palindrome/004-largest-palindrome.spec.js
+++ 004-largest-palindrome/004-largest-palindrome.spec.js
@@ -1,10 +1,10 @@
 import { largestPalindromeProduct } from "./004-largest-palindrome.js";
 
-test("largestPalindrome(2) should return 2", () => {
+test("largestPalindrome(2) should return 9009", () => {
   expect(largestPalindromeProduct(2)).toBe(9009);
 });
 
-test("largestPalindrome(3) should return 3", () => {
+test("largestPalindrome(3) should return 906609", () => {
   expect(largestPalindromeProduct(3)).toBe(906609);
 });
  
